69188724835587aa609f177c1c5361871e3cdf6f
mazeltovik <mazeltovik@gmail.com>
2023-06-13 22:34:21 +0300

    refactor: change solution for copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,5 @@
 import { readdir } from 'node:fs/promises';
-import { createReadStream, createWriteStream} from 'node:fs';
+import { createReadStream, createWriteStream } from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
 import { mkdir, stat, access } from 'node:fs/promises';
@@ -11,22 +11,22 @@ const filesOutput = path.join(__dirname, 'files_copy');
 
 
 const copy = async () => {
-    try {
-        await access(filesInput);
-        await mkdir(filesOutput);
-        const files = await readdir(filesInput);
-        files.forEach(file=>{
-            let filePathInput = path.join(__dirname,'files',file);
-            let filePathOutput = path.join(__dirname,'files_copy',file);
-            let readable = createReadStream(filePathInput,'utf8');
-            readable.on('open',()=>{
-                let writable = createWriteStream(filePathOutput).setDefaultEncoding('utf-8');
-                readable.pipe(writable);
-            })
-        })
-    } catch (err) {
+    await access(filesInput).catch(() => {
         throw new Error('FS operation failed');
-    }
+    })
+    await mkdir(filesOutput).catch(()=>{
+        throw new Error('FS operation failed');
+    })
+    const files = await readdir(filesInput);
+    files.forEach(file => {
+        let filePathInput = path.join(__dirname, 'files', file);
+        let filePathOutput = path.join(__dirname, 'files_copy', file);
+        let readable = createReadStream(filePathInput, 'utf8');
+        readable.on('open', () => {
+            let writable = createWriteStream(filePathOutput).setDefaultEncoding('utf-8');
+            readable.pipe(writable);
+        })
+    })
 };
 
 await copy();
