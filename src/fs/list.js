37cee7e199da25b1ae433db14114d3d4124a54a1
mazeltovik <mazeltovik@gmail.com>
2023-06-14 20:08:24 +0300

    feat:add solution for list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir } from 'node:fs/promises';
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filesPath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    try{
+        const files = await readdir(filesPath);
+        let data = '';
+        files.forEach(file=>data+=`${file}\n`);
+        process.stdout.write(data);
+    } catch{
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
