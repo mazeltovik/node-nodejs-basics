3d3ab6e604e0056257d2e12a4592861c7bcc0da8
mazeltovik <mazeltovik@gmail.com>
2023-06-14 19:49:39 +0300

    feat: add solution for delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { unlink } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const deleteFilePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await unlink(deleteFilePath);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
