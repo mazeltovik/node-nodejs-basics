c99bbb440219ec0f030fd42712c199ce4ee2ea33
mazeltovik <mazeltovik@gmail.com>
2023-06-14 20:20:39 +0300

    feat: add solution for read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { readdir } from 'node:fs/promises';
+import { open } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const fd = await open(filePath);
+        const readable = fd.createReadStream().setEncoding('utf-8');
+        readable.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
