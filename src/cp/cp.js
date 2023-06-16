3e6557dee4cc888a196de66e6a4561e880d11a65
mazeltovik <mazeltovik@gmail.com>
2023-06-16 20:22:35 +0300

    feat: add solution for cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { fork } from 'node:child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork(
+        filePath,
+        args,
+        {
+            stdio: [process.stdin, process.stdout, process.stderr, 'ipc'],
+            encoding: 'utf-8'
+        });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--some-arg value1', '--other 1337', '--arg2 42']);
