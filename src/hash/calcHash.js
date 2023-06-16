06cebb3902c0f2f10f941b9a9c928b2324c74615
mazeltovik <mazeltovik@gmail.com>
2023-06-16 18:04:05 +0300

    feat: add solution for hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileName = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        let data = await readFile(fileName, { encoding: 'utf8' });
+        console.log(createHash('SHA256').update(data).digest('hex'));
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await calculateHash();
