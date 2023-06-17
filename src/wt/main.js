304a96e17a923e2a34d9165e86641660dcbf77ff
Nikita <mazeltovik@gmail.com>
2023-06-17 18:08:58 +0300

    feat: add solution for worker thread task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,48 @@
+import path from "node:path";
+import os from "node:os"
+import { fileURLToPath } from "url";
+import { Worker } from "node:worker_threads";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "worker.js");
+
+class WorkerPool{
+    constructor(filePath,cores){
+        this.filePath = filePath;
+        this.cores = cores;
+        this.workers = [];
+        this.init();
+    }
+    init(){
+        for(let i = 0,nth=10;i<this.cores;i++,nth++){
+            this.workers[i] = new Promise((resolve, reject) => {
+                const worker = new Worker(filePath, {
+                    workerData: nth,
+                });
+                worker.on('message', resolve);
+                worker.on('error', reject);
+            });
+        }
+    }
+    transformResults(data){
+        return data.map(v=>{
+            return {
+                status: v.status == "fulfilled" ? "resolved" : "error",
+                data: v.status == "fulfilled" ? v.value : null
+            }
+        })
+    }
+}
+
 const performCalculations = async () => {
-    // Write your code here
-};
+    const pool = new WorkerPool(filePath,os.availableParallelism());
+    Promise.allSettled(pool.workers).then(data=>{
+        console.log(pool.transformResults(data));
+    });
+}
 
 await performCalculations();
+
+
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,7 +1,9 @@
+import {parentPort,workerData} from 'node:worker_threads'
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
+    parentPort.postMessage(nthFibonacci(workerData));
     // This function sends result of nthFibonacci computations to main thread
 };
 
